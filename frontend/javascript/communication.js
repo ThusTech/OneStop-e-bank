9f901eb72fbc28be72dcc167a94278fdb6f2cc9b
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-08-10 20:40:55 +0200

    Added communication javascript file for API connectivity

---
+++ frontend/javascript/communication.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.login').click(function(){
+        const email = $("#email").val();
+        // $.ajax({
+        //     url:,
+        //     type: 'POST',
+        //     contentType: 'application/json',
+        //     data: JSON.stringify({ data: email}),
+        //     success:,
+        //     error:,
+        // })
+    })
+})
